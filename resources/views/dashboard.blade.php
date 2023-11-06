92d31ef0d164b924cdd06f52f730c7332a1d300b
hera <doraotari@gmail.com>
2023-11-06 17:19:28 +0700

    pertemuan 3

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.tema')
+@section('konten')
+    <div class="row">
+        <div class="col-lg-4">
+            ini dashboard
+        </div>
+        <div class="col-lg-5">
+            <img src="" alt="">
+        </div>
+    </div>
+@endsection
